3ebdd53d674591812e96705147eeba2df62975fa
drubach <daniel.c.rubach@gmail.com>
2021-12-20 05:28:55 -0500

    turned debug off

--- app.py
+++ app.py
@@ -198,4 +198,4 @@ def logout():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True) ##############################Turn off for submission!!!!!
+            debug=False) ##### True for dev work#.
